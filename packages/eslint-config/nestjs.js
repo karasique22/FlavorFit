ec13c82f718461d07ca758decc0c4dd94c8ab3c9
karasique22 <timofeidudin@mail.ru>
2026-01-31 17:35:10 +0300

    build: configure nestjs eslint and typescript configs

---
+++ packages/eslint-config/nestjs.js
@@ -0,0 +1,46 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import turboPlugin from "eslint-plugin-turbo";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+/**
+ * ESLint configuration for NestJS applications.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ */
+export const config = [
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      turbo: turboPlugin,
+    },
+    rules: {
+      "turbo/no-undeclared-env-vars": "warn",
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: "commonjs",
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-floating-promises": "warn",
+      "@typescript-eslint/no-unsafe-argument": "warn",
+    },
+  },
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
+];
